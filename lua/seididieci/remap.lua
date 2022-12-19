df04ba34607b2670a030995e87df3791b186680f
seididieci <brumiano@gmail.com>
2022-12-19 12:27:48 +0100

    fix move selection keymaps

--- lua/seididieci/remap.lua
+++ lua/seididieci/remap.lua
@@ -1,9 +1,8 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
-vim.keymap.set("v", "J", ":m >+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m <-2<CR>gv=gv")
-
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
 vim.keymap.set("n", "Y", "yg$")
 vim.keymap.set("n", "J", "mzJ`z")
@@ -27,7 +26,7 @@ vim.keymap.set("i", "<C-c>", "<ESC>")
 vim.keymap.set("n", "Q", "<nop>")
 vim.keymap.set("n", "<C-f>", "<cmd> silent !tmux neww tmux-sessionizer<CR>")
 vim.keymap.set("n", "<leader>f", function ()
- vim.lsp.buf.format() 
+ vim.lsp.buf.format()
 end)
 
 vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
