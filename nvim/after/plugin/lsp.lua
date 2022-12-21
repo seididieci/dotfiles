252e5233dc1fdf9b978ce3ec917835ed27a4808c
seididieci <brumiano@gmail.com>
2022-12-21 10:50:23 +0100

    Fixes on lsp configurations

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -11,7 +11,7 @@ lsp.ensure_installed({
 
 local cmp = require('cmp')
 local cmp_select = {behaviour = cmp.SelectBehavior.Select}
-local cmp_mappins = lsp.defaults.cmp_mappings({
+local cmp_mappings = lsp.defaults.cmp_mappings({
 	['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
 	['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
 	['<C-y>'] = cmp.mapping.confirm({ select = true }),
@@ -27,14 +27,14 @@ lsp.setup_nvim_cmp({
 })
 
 lsp.on_attach(function(client, bufnr)
-	local opts = {buffer = buffer, remap = false}
+	local opts = {buffer = bufnr, remap = false}
 
 	vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
 	vim.keymap.set("n", "K", function() vim.lsp.buf.over() end, opts)
 	vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
-	vim.keymap.set("n", "<leader>vd", function() vim.lsp.buf.open_float() end, opts)
-	vim.keymap.set("n", "[d", function() vim.lsp.buf.goto_next() end, opts)
-	vim.keymap.set("n", "]d", function() vim.lsp.buf.goto_prev() end, opts)
+	vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
+	vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
+	vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
 	vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
 	vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
 	vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
@@ -43,3 +43,8 @@ end)
 
 lsp.setup()
 
+vim.diagnostic.config({
+  virtual_text = true,
+  signs = true,
+  underline = true,
+})
