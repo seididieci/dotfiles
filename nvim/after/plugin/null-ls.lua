61103638eb1e075f347d60719786ea6958dd3d04
seididieci <brumiano@gmail.com>
2023-04-14 14:55:50 +0200

    Add null-ls configuration

--- nvim/after/plugin/null-ls.lua
+++ nvim/after/plugin/null-ls.lua
@@ -3,7 +3,9 @@ local null_ls = require('null-ls')
 null_ls.setup({
   sources = {
     null_ls.builtins.formatting.stylua,
-    null_ls.builtins.formatting.prettier,
+    null_ls.builtins.formatting.prettierd.with({
+      filetypes = { "html", "vue", "svelte" },
+    }),
     null_ls.builtins.diagnostics.eslint,
   },
 })
