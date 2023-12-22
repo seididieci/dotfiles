252b8a1dca87aca45584d2a42acad3c2b42c00cb
seididieci <brumiano@gmail.com>
2023-12-22 08:34:14 +0100

    Migrate to harpoon2.

--- nvim/lua/seididieci/packer.lua
+++ nvim/lua/seididieci/packer.lua
@@ -23,7 +23,10 @@ return require('packer').startup(function(use)
 
   use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
   use('nvim-treesitter/nvim-treesitter-context')
-  use('ThePrimeagen/harpoon')
+  use {
+    "ThePrimeagen/harpoon",
+    branch = "harpoon2",
+  }
   use('mbbill/undotree')
   use('tpope/vim-fugitive')
 
