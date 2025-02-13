6dc1e399de255d4da426a5e210aa86fef69e2931
seididieci <brumiano@gmail.com>
2025-02-13 16:56:32 +0100

    Fix colors and telescope borders.

--- nvim/after/plugin/colors.lua
+++ nvim/after/plugin/colors.lua
@@ -6,14 +6,12 @@ function ColorMyPencils(color)
 	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 end
 
---ColorMyPencils()
 require("cyberdream").setup({
 	transparent = false,
 	italic_comments = true,
 	hide_fillchars = true,
-	borderless_telescope = false,
+	borderless_pickers = false,
 	terminal_colors = true,
 })
-vim.cmd("colorscheme cyberdream") -- set the colorscheme
-vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+
+ColorMyPencils("cyberdream")
