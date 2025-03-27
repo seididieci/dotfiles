80883b4ff59032ed15d53b35d016fe6b036eb0aa
seididieci <brumiano@gmail.com>
2025-03-27 13:14:28 +0100

    Fix some dap symbols

--- nvim/after/plugin/dap.lua
+++ nvim/after/plugin/dap.lua
@@ -26,8 +26,8 @@ vim.api.nvim_set_hl(0, 'DapBreakpoint', { ctermbg = 0, fg = '#993939', bg = '#18
 vim.api.nvim_set_hl(0, 'DapLogPoint', { ctermbg = 0, fg = '#61afef', bg = '#181622' })
 vim.api.nvim_set_hl(0, 'DapStopped', { ctermbg = 0, fg = '#98c379', bg = '#31353f' })
 
-vim.fn.sign_define('DapBreakpoint', { text = '', texthl = 'DapBreakpoint' })
-vim.fn.sign_define('DapBreakpointCondition', { text = 'ﳁ', texthl = 'DapBreakpoint' })
+vim.fn.sign_define('DapBreakpoint', { text = '', texthl = 'DapBreakpoint' })
+vim.fn.sign_define('DapBreakpointCondition', { text = '', texthl = 'DapBreakpoint' })
 vim.fn.sign_define('DapBreakpointRejected', { text = '', texthl = 'DapBreakpoint' })
 vim.fn.sign_define('DapLogPoint', { text = '', texthl = 'DapLogPoint' })
 vim.fn.sign_define('DapStopped', { text = '', texthl = 'DapStopped', linehl = 'DapStopped', numhl = 'DapStopped' })
@@ -91,14 +91,14 @@ dapui.setup({
     -- Display controls in this element
     element = "repl",
     icons = {
-      pause = "",
-      play = "",
-      step_into = "",
-      step_over = "",
-      step_out = "",
+      pause = "",
+      play = "",
+      step_into = "",
+      step_over = "",
+      step_out = "",
       step_back = "",
-      run_last = "↻",
-      terminate = "□",
+      run_last = "",
+      terminate = "",
     },
   },
   floating = {
