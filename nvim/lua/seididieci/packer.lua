85e4c261fecd131f34f48f9ac3a107483799ad14
seididieci <brumiano@gmail.com>
2025-04-22 10:36:35 +0200

    Use conform intsead of lsp formatters

--- nvim/lua/seididieci/packer.lua
+++ nvim/lua/seididieci/packer.lua
@@ -4,113 +4,112 @@
 vim.cmd([[packadd packer.nvim]])
 
 return require("packer").startup(function(use)
-	-- Packer can manage itself
-	use("wbthomason/packer.nvim")
-	use("rstacruz/vim-closer")
-	use({ "kyazdani42/nvim-web-devicons" })
+  -- Packer can manage itself
+  use("wbthomason/packer.nvim")
+  use("rstacruz/vim-closer")
+  use({ "kyazdani42/nvim-web-devicons" })
 
-	use({
-		"nvim-telescope/telescope.nvim",
-		requires = { { "nvim-lua/plenary.nvim" } },
-	})
+  use({
+    "nvim-telescope/telescope.nvim",
+    requires = { { "nvim-lua/plenary.nvim" } },
+  })
 
-	-- Color Schemes
-	use({ "rose-pine/neovim", as = "rose-pine" })
-	use({ "bluz71/vim-moonfly-colors", as = "moonfly" })
-	use({ "scottmckendry/cyberdream.nvim" })
-	use({ "projekt0n/github-nvim-theme" })
-	use({ "EdenEast/nightfox.nvim" })
+  -- Color Schemes
+  use({ "rose-pine/neovim", as = "rose-pine" })
+  use({ "bluz71/vim-moonfly-colors", as = "moonfly" })
+  use({ "scottmckendry/cyberdream.nvim" })
+  use({ "projekt0n/github-nvim-theme" })
+  use({ "EdenEast/nightfox.nvim" })
 
-	-- Treesitter
-	use("nvim-treesitter/nvim-treesitter", { run = ":TSUpdate" })
-	use({
-		"nvim-treesitter/nvim-treesitter-context",
-		requires = {
-			"nvim-treesitter/nvim-treesitter",
-		},
-	})
+  -- Treesitter
+  use("nvim-treesitter/nvim-treesitter", { run = ":TSUpdate" })
+  use({
+    "nvim-treesitter/nvim-treesitter-context",
+    requires = {
+      "nvim-treesitter/nvim-treesitter",
+    },
+  })
 
-	use({ "ThePrimeagen/harpoon", branch = "harpoon2" })
-	use("mbbill/undotree")
-	use("tpope/vim-fugitive")
-	use({
-		"kdheepak/lazygit.nvim",
-		-- optional for floating window border decoration
-		requires = {
-			"nvim-lua/plenary.nvim",
-		},
-	})
+  use({ "ThePrimeagen/harpoon", branch = "harpoon2" })
+  use("mbbill/undotree")
+  use("tpope/vim-fugitive")
+  use({
+    "kdheepak/lazygit.nvim",
+    -- optional for floating window border decoration
+    requires = {
+      "nvim-lua/plenary.nvim",
+    },
+  })
 
-	use({
-		"VonHeikemen/lsp-zero.nvim",
-		requires = {
-			-- LSP Support
-			{ "neovim/nvim-lspconfig" },
-			{ "williamboman/mason.nvim" },
-			{ "williamboman/mason-lspconfig.nvim" },
-			{ "artemave/workspace-diagnostics.nvim" },
+  use({
+    "VonHeikemen/lsp-zero.nvim",
+    requires = {
+      -- LSP Support
+      { "neovim/nvim-lspconfig" },
+      { "williamboman/mason.nvim" },
+      { "williamboman/mason-lspconfig.nvim" },
+      { "artemave/workspace-diagnostics.nvim" },
 
-			-- Autocompletion
-			{ "hrsh7th/nvim-cmp" },
-			{ "hrsh7th/cmp-buffer" },
-			{ "hrsh7th/cmp-path" },
-			{ "saadparwaiz1/cmp_luasnip" },
-			{ "hrsh7th/cmp-nvim-lsp" },
-			{ "hrsh7th/cmp-nvim-lua" },
-			{ "onsails/lspkind.nvim" },
-			{ "hrsh7th/cmp-nvim-lsp-signature-help" },
+      -- Autocompletion
+      { "hrsh7th/nvim-cmp" },
+      { "hrsh7th/cmp-buffer" },
+      { "hrsh7th/cmp-path" },
+      { "saadparwaiz1/cmp_luasnip" },
+      { "hrsh7th/cmp-nvim-lsp" },
+      { "hrsh7th/cmp-nvim-lua" },
+      { "onsails/lspkind.nvim" },
+      { "hrsh7th/cmp-nvim-lsp-signature-help" },
 
-			-- Snippets
-			{ "L3MON4D3/LuaSnip" },
-			{ "rafamadriz/friendly-snippets" },
-		},
-	})
+      -- Snippets
+      { "L3MON4D3/LuaSnip" },
+      { "rafamadriz/friendly-snippets" },
+    },
+  })
 
-	-- This is for the C# LSP Go to Defitinion on non-project files
-	use({
-		"Decodetalkers/csharpls-extended-lsp.nvim",
-	})
+  -- This is for the C# LSP Go to Defitinion on non-project files
+  use({
+    "Decodetalkers/csharpls-extended-lsp.nvim",
+  })
 
-	use({
-		"nvim-lualine/lualine.nvim",
-		requires = { "kyazdani42/nvim-web-devicons" },
-	})
+  use({
+    "nvim-lualine/lualine.nvim",
+    requires = { "kyazdani42/nvim-web-devicons" },
+  })
 
-	use("lewis6991/gitsigns.nvim")
+  use("lewis6991/gitsigns.nvim")
 
-	use({
-		"vuki656/package-info.nvim",
-		requires = "MunifTanjim/nui.nvim",
-	})
+  use({
+    "vuki656/package-info.nvim",
+    requires = "MunifTanjim/nui.nvim",
+  })
 
-	-- use({
-	-- 	"jose-elias-alvarez/null-ls.nvim",
-	-- 	requires = "nvim-lua/plenary.nvim",
-	-- })
+  use({
+    "stevearc/conform.nvim",
+  })
 
-	use("ThePrimeagen/vim-be-good")
+  use("ThePrimeagen/vim-be-good")
 
-	--- Debugging...
-	--use('puremourning/vimspector')
-	use("mfussenegger/nvim-dap")
-	use({
-		"rcarriga/nvim-dap-ui",
-		requires = {
-			"mfussenegger/nvim-dap",
-			"nvim-neotest/nvim-nio",
-		},
-	})
+  --- Debugging...
+  --use('puremourning/vimspector')
+  use("mfussenegger/nvim-dap")
+  use({
+    "rcarriga/nvim-dap-ui",
+    requires = {
+      "mfussenegger/nvim-dap",
+      "nvim-neotest/nvim-nio",
+    },
+  })
 
-	-- Some useless cool stuff
-	use("mattn/calendar-vim")
+  -- Some useless cool stuff
+  use("mattn/calendar-vim")
 
-	use({
-		"Exafunction/codeium.vim",
-		requires = {
-			"nvim-lua/plenary.nvim",
-			"hrsh7th/nvim-cmp",
-		},
-	})
+  use({
+    "Exafunction/codeium.vim",
+    requires = {
+      "nvim-lua/plenary.nvim",
+      "hrsh7th/nvim-cmp",
+    },
+  })
 
-	use("sindrets/diffview.nvim")
+  use("sindrets/diffview.nvim")
 end)
