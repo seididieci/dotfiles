e2c03de28154f75de5f66e1eebd235cf4d64bfd9
seididieci <brumiano@gmail.com>
2025-04-22 11:17:34 +0200

    Fix typescript formatter config

--- nvim/after/plugin/conform.lua
+++ nvim/after/plugin/conform.lua
@@ -7,5 +7,6 @@ conform.setup({
 		rust = { "rustfmt", lsp_format = "fallback" },
 		--javascript = { "prettierd", "prettier", stop_after_first = true },
 		vue = { "prettierd", "volar", stop_after_first = true },
+		typescript = { lsp_format = "true" },
 	},
 })
