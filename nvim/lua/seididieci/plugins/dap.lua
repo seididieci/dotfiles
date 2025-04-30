b691bd123aa4c41d53e018983d6dead7fe9aad94
seididieci <brumiano@gmail.com>
2025-04-30 16:22:50 +0200

    Fix dapui

--- nvim/lua/seididieci/plugins/dap.lua
+++ nvim/lua/seididieci/plugins/dap.lua
@@ -43,8 +43,92 @@ return {
       },
     }
 
+    -- Set symbols for debugger
+    vim.api.nvim_set_hl(0, 'DapBreakpoint', { ctermbg = 0, fg = '#993939', bg = '#181622' })
+    vim.api.nvim_set_hl(0, 'DapLogPoint', { ctermbg = 0, fg = '#61afef', bg = '#181622' })
+    vim.api.nvim_set_hl(0, 'DapStopped', { ctermbg = 0, fg = '#98c379', bg = '#31353f' })
+
+    vim.fn.sign_define('DapBreakpoint', { text = '', texthl = 'DapBreakpoint' })
+    vim.fn.sign_define('DapBreakpointCondition', { text = '', texthl = 'DapBreakpoint' })
+    vim.fn.sign_define('DapBreakpointRejected', { text = '', texthl = 'DapBreakpoint' })
+    vim.fn.sign_define('DapLogPoint', { text = '', texthl = 'DapLogPoint' })
+    vim.fn.sign_define('DapStopped', { text = '', texthl = 'DapStopped', linehl = 'DapStopped', numhl = 'DapStopped' })
+
     local dapui = require('dapui')
-    dapui.setup({})
+    dapui.setup({
+      icons = { expanded = "▾", collapsed = "▸", current_frame = "▸" },
+      mappings = {
+        -- Use a table to apply multiple mappings
+        expand = { "<CR>", "<2-LeftMouse>" },
+        open = "o",
+        remove = "d",
+        edit = "e",
+        repl = "r",
+        toggle = "t",
+      },
+      -- Use this to override mappings for specific elements
+      element_mappings = {
+        -- Example:
+        -- stacks = {
+        --   open = "<CR>",
+        --   expand = "o",
+        -- }
+      },
+      -- Expand lines larger than the window
+      -- Requires >= 0.7
+      expand_lines = vim.fn.has("nvim-0.7") == 1,
+      layouts = {
+        {
+          elements = {
+            -- Elements can be strings or table with id and size keys.
+            { id = "scopes", size = 0.25 },
+            "breakpoints",
+            "stacks",
+            "watches",
+          },
+          size = 50, -- 40 columns
+          position = "right",
+        },
+        {
+          elements = {
+            -- "console",
+            "repl",
+          },
+          size = 0.25, -- 25% of total lines
+          position = "bottom",
+        },
+      },
+      controls = {
+        -- Requires Neovim nightly (or 0.8 when released)
+        enabled = true,
+        -- Display controls in this element
+        element = "repl",
+        icons = {
+          pause = "",
+          play = "",
+          step_into = "",
+          step_over = "",
+          step_out = "",
+          step_back = "",
+          run_last = "",
+          terminate = "",
+        },
+      },
+      floating = {
+        max_height = nil,  -- These can be integers or a float between 0 and 1.
+        max_width = nil,   -- Floats will be treated as percentage of your screen.
+        border = "single", -- Border style. Can be "single", "double" or "rounded"
+        mappings = {
+          close = { "q", "<Esc>" },
+        },
+      },
+      windows = { indent = 1 },
+      render = {
+        max_type_length = nil, -- Can be integer or nil.
+        max_value_lines = 100, -- Can be integer or nil.
+      }
+    })
+
     dap.listeners.before.attach["dapui_config"] = function()
       dapui.open()
     end
@@ -73,5 +157,7 @@ return {
       dapui.eval()
       dapui.eval()
     end)
+
+    require("nvim-dap-virtual-text").setup()
   end
 }
