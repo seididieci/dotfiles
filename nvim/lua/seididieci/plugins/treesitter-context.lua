5d6138315b0c89aeaf82a7b0ce6370e70d1d560b
seididieci <brumiano@gmail.com>
2025-05-06 10:15:55 +0200

    Limit treesitter-context height

--- nvim/lua/seididieci/plugins/treesitter-context.lua
+++ nvim/lua/seididieci/plugins/treesitter-context.lua
@@ -3,4 +3,13 @@ return {
   dependencies = {
     "nvim-treesitter/nvim-treesitter",
   },
+  config = function()
+    require("treesitter-context").setup({
+      enable = true,
+      max_lines = 4,
+      trim_scope = "outer",
+      min_window_height = 0,
+      multiline_threshold = 8,
+    })
+  end,
 }
