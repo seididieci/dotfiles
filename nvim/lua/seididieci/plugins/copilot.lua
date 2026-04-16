8d5498009b9f89874276df2579c1ae50fdbe6e6a
seididieci <brumiano@gmail.com>
2026-04-16 17:35:10 +0200

    fix(nvim): fix copilot accept keybinding.

--- nvim/lua/seididieci/plugins/copilot.lua
+++ nvim/lua/seididieci/plugins/copilot.lua
@@ -1,9 +1,12 @@
 return {
-  'github/copilot.vim',
-  event = 'InsertEnter',
-  config = function()
-    vim.g.copilot_no_tab_map = true
-    --vim.g.copilot_assume_mapped = true
-    vim.g.copilot_tab_fallback = ''
-  end
+	"github/copilot.vim",
+	event = "InsertEnter",
+	config = function()
+		vim.g.copilot_no_tab_map = true
+		--vim.g.copilot_assume_mapped = true
+		vim.g.copilot_tab_fallback = ""
+		vim.keymap.set("i", "<C-g>", function()
+			return vim.fn["copilot#Accept"]("\\<S-Tab>")
+		end, { expr = true, silent = true, replace_keycodes = false })
+	end,
 }
