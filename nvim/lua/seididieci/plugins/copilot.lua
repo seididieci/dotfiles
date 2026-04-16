3f54d6f8266048011b91fce9d9999b6765b430cd
seididieci <brumiano@gmail.com>
2026-04-16 13:04:56 +0200

    feat: replace windsurf with Copilot

---
+++ nvim/lua/seididieci/plugins/copilot.lua
@@ -0,0 +1,9 @@
+return {
+  'github/copilot.vim',
+  event = 'InsertEnter',
+  config = function()
+    vim.g.copilot_no_tab_map = true
+    --vim.g.copilot_assume_mapped = true
+    vim.g.copilot_tab_fallback = ''
+  end
+}
--- nvim/lua/seididieci/plugins/windsurf.lua
+++ nvim/lua/seididieci/plugins/windsurf.lua
@@ -1,23 +1,24 @@
-return {
-  'Exafunction/windsurf.vim',
-  event = 'BufEnter',
-  config = function()
-    vim.g.codeium_no_map_tab = 1
-
-    vim.keymap.set("i", "<C-g>", function()
-      return vim.fn["codeium#Accept"]()
-    end, { expr = true, silent = true })
-
-    vim.keymap.set("i", "<C-.>", function()
-      vim.fn["codeium#AcceptNextWord"]()
-    end, { expr = true, silent = true })
-
-    vim.keymap.set("i", "<C-,>", function()
-      vim.fn["codeium#AcceptNextLine"]()
-    end, { expr = true, silent = true })
-
-    vim.keymap.set("i", "<C-x>", function()
-      return vim.fn["codeium#Clear"]()
-    end, { expr = true, silent = true })
-  end
-}
+return {}
+-- return {
+--   'Exafunction/windsurf.vim',
+--   event = 'BufEnter',
+--   config = function()
+--     vim.g.codeium_no_map_tab = 1
+--
+--     vim.keymap.set("i", "<C-g>", function()
+--       return vim.fn["codeium#Accept"]()
+--     end, { expr = true, silent = true })
+--
+--     vim.keymap.set("i", "<C-.>", function()
+--       vim.fn["codeium#AcceptNextWord"]()
+--     end, { expr = true, silent = true })
+--
+--     vim.keymap.set("i", "<C-,>", function()
+--       vim.fn["codeium#AcceptNextLine"]()
+--     end, { expr = true, silent = true })
+--
+--     vim.keymap.set("i", "<C-x>", function()
+--       return vim.fn["codeium#Clear"]()
+--     end, { expr = true, silent = true })
+--   end
+-- }
